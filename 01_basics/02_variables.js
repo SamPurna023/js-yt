79718c00bdd3202da7f764dfe6a5182fe1915916
SamPurna023 <156777916+SamPurna023@users.noreply.github.com>
2024-07-14 16:02:28 +0000

    datatypes

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("hello") //we are using nodejs, not browser.
+
+console.log(3+3)
+
+console.log("Sampurna")
+
+let name="Sampurna"
+let age= 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null // it is an object
+// undefined=>
+// symbol => unique
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
